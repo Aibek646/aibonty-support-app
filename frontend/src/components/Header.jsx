e88874b6d2931b737fad9fe716ea9b779cbb4d49
Aibek Ramazanov <aibekramazanov@Aibeks-MBP.lan>
2022-11-22 17:03:38 -0500

    finish authentication

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,18 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
+import { logout, reset } from "../features/auth/authSlice";
 
 const Header = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.auth);
+
+    const onLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+        navigate("/");
+    };
     return (
         <>
             <header className="header">
@@ -13,20 +21,27 @@ const Header = () => {
                 </div>
 
                 <ul>
-                    {/* <ul>{user ? <li></li> : <li></li>}</ul> */}
-                    <>
+                    {user ? (
                         <li>
-                            <Link to="/login">
-                                <FaSignInAlt /> Login
-                            </Link>
+                            <button onClick={onLogout} className="btn">
+                                <FaSignOutAlt /> Logout
+                            </button>
                         </li>
+                    ) : (
+                        <>
+                            <li>
+                                <Link to="/login">
+                                    <FaSignInAlt /> Login
+                                </Link>
+                            </li>
 
-                        <li>
-                            <Link to="/register">
-                                <FaSignOutAlt /> Register
-                            </Link>
-                        </li>
-                    </>
+                            <li>
+                                <Link to="/register">
+                                    <FaSignOutAlt /> Register
+                                </Link>
+                            </li>
+                        </>
+                    )}
                 </ul>
             </header>
         </>
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -7,13 +7,30 @@ const register = async (userData) => {
     const response = await axios.post(API_URL, userData);
 
     if (response.data) {
-        console.log(response.data);
+        localStorage.setItem("user", JSON.stringify(response.data));
     }
     return response.data;
 };
 
+//Login user
+
+const login = async (userData) => {
+    const response = await axios.post(API_URL + "login", userData);
+
+    if (response.data) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
+};
+
+//Logout user
+
+const logout = () => localStorage.removeItem("user");
+
 const authService = {
-    register
+    register,
+    login,
+    logout
 };
 
 export default authService;
