0a39a054101c0781fcbf6f830963e224c9ab8206
Aibek Ramazanov <aibekramazanov@Aibeks-MacBook-Pro.local>
2022-12-08 08:45:39 -0600

    Private route fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,21 +20,31 @@ function App() {
                         <Route path="/" element={<Home />} />
                         <Route path="/login" element={<Login />} />
                         <Route path="/register" element={<Register />} />
-                        <Route path="/tickets" element={<PrivateRoute />}>
-                            <Route path="/tickets" element={<Tickets />} />
-                        </Route>
-                        <Route path="/new-ticket" element={<PrivateRoute />}>
-                            <Route path="/new-ticket" element={<NewTicket />} />
-                        </Route>
-                        {/* <Route
-                            path="/ticket/:tickedId"
-                            element={<PrivateRoute />}
-                        >
-                            <Route
-                                path="/ticket/:ticketId"
-                                element={<Ticket />}
-                            />
-                        </Route> */}
+                        <Route
+                            path="/new-ticket"
+                            element={
+                                <PrivateRoute>
+                                    <NewTicket />
+                                </PrivateRoute>
+                            }
+                        />
+
+                        <Route
+                            path="/tickets"
+                            element={
+                                <PrivateRoute>
+                                    <Tickets />
+                                </PrivateRoute>
+                            }
+                        />
+                        <Route
+                            path="/ticket/:ticketId"
+                            element={
+                                <PrivateRoute>
+                                    <Ticket />
+                                </PrivateRoute>
+                            }
+                        />
                     </Routes>
                 </div>
             </Router>
